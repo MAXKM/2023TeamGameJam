b37dbbe18260ca9da23d9fdfb1ffad1d0f06941a
yamaryo <toumakanaya@gmail.com>
2023-02-07 02:06:56 +0900

    プレハブとスクリプトを追加しました。

---
+++ Assets/yamaryo(select)/movebuttonScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movebuttonScript : MonoBehaviour
+{
+    public bool movebtn; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClicked()
+    {
+        movebtn = true;
+    }
+
+}
