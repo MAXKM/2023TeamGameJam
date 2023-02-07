e4aac822495103d7af3640dbb78812b416c26c28
2223116-PC\2223116 <s2223116@cco.kanagawa-it.ac.jp>
2023-02-07 12:52:47 +0900

    修正しました

--- Assets/Ayaka/bulletController.cs
+++ Assets/Ayaka/bulletController.cs
@@ -4,28 +4,25 @@ using UnityEngine;
 
 public class bulletController : MonoBehaviour
 {
-    /*
     public void Shoot(Vector3 power)
     {
-        GetComponent<Rigitbody>().AddForce(power);
+        GetComponent<Rigidbody>().AddForce(power);
     }
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 60;
-        Shoot(new Vector3(0, 200, 2000));
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ìñÇΩÇ¡ÇΩÇÁìIè¡é∏
-        void OnCollisionEnter(Collider other)
+        //âÊñ äOÇ…èoÇΩÇÁãÖÇÕè¡ñ≈
+        if (transform.position.y < -50)
         {
             Destroy(gameObject);
         }
     }
-    */
 }
